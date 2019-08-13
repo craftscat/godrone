4ee712be74109182abd241c7f42265507180493e
taqboz <umibooose@gmail.com>
2019-08-13 09:04:41 +0900

    logging setting

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/taqboz/gotello/app/models"
+	"github.com/taqboz/gotello/config"
+	"github.com/taqboz/gotello/utils"
 	"time"
 )
 
 func main()  {
+	utils.LoggingSetting(config.Config.LogFile)
 	droneManager := models.NewDroneManager()
 	droneManager.TakeOff()
 	time.Sleep(10*time.Second)
